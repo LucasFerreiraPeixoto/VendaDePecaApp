d6cf52c70976b41a9439d8ad13779570fc7025e3
Lucas Ferreira Peixoto <lucas2005faculdade@gmail.com>
2025-09-08 15:46:12 -0300

    Foi criado a struct de peça.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,6 +8,25 @@ namespace ConsoleApp1
 {
     internal class Program
     {
+        //váriavel para receber conteúdos em arquivos
+        static string arquivopeças = @"C:\Users\USER\OneDrive\Desktop\faculdade C#\Projeto Venda de Peças\CodigoProgramaDeVendas\registroPecas.txt";
+        static string arquivovendas = @"C:\Users\USER\OneDrive\Desktop\faculdade C#\Projeto Venda de Peças\CodigoProgramaDeVendas\registroVendas.txt";
+
+        public struct peça
+        {
+            public string nome;
+            public int quant;
+            public string tipo;
+            public float preço;
+
+            public peça(string nome, int quantidade, string tipo, float preço)
+            {
+                this.nome = nome;
+                this.quant = quantidade;
+                this .tipo = tipo;
+                this.preço = preço;
+            }
+        }
         static void Main(string[] args)
         {
         }
